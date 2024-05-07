f7d8edc913b5095d0d0223430070cb82efd14621
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-05-06 23:57:53 -0300

    Finalizacion ejercicio07

--- src/ar/edu/unju/fi/ejercicio07/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio07/main/Main.java
@@ -16,6 +16,27 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner (System.in);
+		List<Producto> productos = new ArrayList<>();
+		
+		    productos.add(new Producto("001", "Producto 1", 10.5, Producto.OrigenFabricacion.ARGENTINA, Producto.Categoria.ELECTROHOGAR, true));
+	        productos.add(new Producto("002", "Producto 2", 20.75, Producto.OrigenFabricacion.CHINA, Producto.Categoria.INFORMATICA, true));
+	        productos.add(new Producto("003", "Producto 3", 15.0, Producto.OrigenFabricacion.BRASIL, Producto.Categoria.HERRAMIENTAS, true));
+	        productos.add(new Producto("004", "Producto 4", 30.25, Producto.OrigenFabricacion.URUGUAY, Producto.Categoria.TELEFONIA, false));
+	        productos.add(new Producto("005", "Producto 5", 12.99, Producto.OrigenFabricacion.ARGENTINA, Producto.Categoria.ELECTROHOGAR, true));
+	        productos.add(new Producto("006", "Producto 6", 18.49, Producto.OrigenFabricacion.CHINA, Producto.Categoria.INFORMATICA, true));
+	        productos.add(new Producto("007", "Producto 7", 22.75, Producto.OrigenFabricacion.BRASIL, Producto.Categoria.HERRAMIENTAS, false));
+	        productos.add(new Producto("008", "Producto 8", 35.0, Producto.OrigenFabricacion.URUGUAY, Producto.Categoria.TELEFONIA, true));
+	        productos.add(new Producto("009", "Producto 9", 8.75, Producto.OrigenFabricacion.ARGENTINA, Producto.Categoria.ELECTROHOGAR, false));
+	        productos.add(new Producto("010", "Producto 10", 19.99, Producto.OrigenFabricacion.CHINA, Producto.Categoria.INFORMATICA, true));
+	        productos.add(new Producto("011", "Producto 11", 16.5, Producto.OrigenFabricacion.BRASIL, Producto.Categoria.HERRAMIENTAS, true));
+	        productos.add(new Producto("012", "Producto 12", 27.25, Producto.OrigenFabricacion.URUGUAY, Producto.Categoria.TELEFONIA, true));
+	        productos.add(new Producto("013", "Producto 13", 9.99, Producto.OrigenFabricacion.ARGENTINA, Producto.Categoria.ELECTROHOGAR, true));
+	        productos.add(new Producto("014", "Producto 14", 21.75, Producto.OrigenFabricacion.CHINA, Producto.Categoria.INFORMATICA, false));
+	        productos.add(new Producto("015", "Producto 15", 14.0, Producto.OrigenFabricacion.BRASIL, Producto.Categoria.HERRAMIENTAS, true));
+
+	        for (Producto producto : productos) {
+	            System.out.println(producto.toString());
+	        }
 		
 		 boolean continuar = true;
 	        while (continuar) {
@@ -49,7 +70,7 @@ public class Main {
 	                case 3:
 	                    // Utilizar Function, map() para incrementar precios en un 20%
 	                    Function<Producto, Producto> incrementarPrecio = p -> {
-	                        p.setPrecio(p.getPrecio() * 1.20);
+	                        p.setPrecioUnitario(p.getPrecioUnitario() * 1.20);
 	                        return p;
 	                    };
 	                    List<Producto> productosIncrementados = productos.stream().map(incrementarPrecio).collect(Collectors.toList());
@@ -63,13 +84,13 @@ public class Main {
 	                    break;
 	                case 5:
 	                    // Ordenar productos por precio de forma descendente
-	                    productos.sort(Comparator.comparing(Producto::getPrecio).reversed());
+	                    productos.sort(Comparator.comparing(Producto::getPrecioUnitario).reversed());
 	                    productos.forEach(System.out::println);
 	                    break;
 	                case 6:
 	                    // Utilizar Function y map() para mostrar nombres en mayúsculas
 	                    Function<Producto, Producto> nombreMayusculas = p -> {
-	                        p.setNombre(p.getNombre().toUpperCase());
+	                        p.setDescripcion(p.getDescripcion().toUpperCase());
 	                        return p;
 	                    };
 	                    List<Producto> productosMayusculas = productos.stream().map(nombreMayusculas).collect(Collectors.toList());
@@ -80,7 +101,8 @@ public class Main {
 	                    break;
 	            }
 	        }
-	    
+	    scanner.close();
 	}
+	
 
 }
